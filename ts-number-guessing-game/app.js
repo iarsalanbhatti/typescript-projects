3c5e437bffb34d97df3c581af7d1cef6fd408183
Arxalan <arxalanbhatti@gmail.com>
2024-03-26 01:51:37 +0500

    updated files

--- ts-number-guessing-game/app.js
+++ ts-number-guessing-game/app.js
@@ -1,13 +1,16 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const randNum = Math.floor(Math.random() * 10) + 1;
-let allowedAttempts = 3;
-console.log(randNum);
 console.log("Number Guessing Game!");
 console.log("You have 3 attempts to guess the correct number between 1 and 10.");
 async function guessTheNum() {
+    const randNum = Math.floor(Math.random() * 10) + 1;
+    let allowedAttempts = 3;
     while (allowedAttempts > 0) {
-        const getUserInput = await inquirer.prompt({ message: "Please enter first number", type: "number", name: "userGuessedNum" });
+        const getUserInput = await inquirer.prompt({
+            message: "Please enter first number",
+            type: "number",
+            name: "userGuessedNum",
+        });
         if (getUserInput.userGuessedNum < 1 || getUserInput.userGuessedNum > 10) {
             console.log("Please enter a valid number between 1 and 10.");
         }
